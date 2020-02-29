cb39bf1af4fc061e9fc2d70604f35510b95bc911
Adarsh4321 <61613380+Adarsh4321@users.noreply.github.com>
2020-02-29 15:36:56 +0530

    variable sum1 to s

--- addtion3.c
+++ addtion3.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 int main() {    
 
-    int number1, number2, sum1;
+    int number1, number2, s;
     
     printf("Enter two integers: ");
     scanf("%d %d", &number1, &number2);
 
     // calculating sum
-    sum1 = number1 + number2;      
+    s = number1 + number2;      
     
-    printf("%d + %d = %d", number1, number2, sum1);
+    printf("%d + %d = %d", number1, number2, s);
     return 0;
 }
 
