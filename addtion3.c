4b3d86b88df1d69e517fdf73423262d26c98225b
Adarsh4321 <61613380+Adarsh4321@users.noreply.github.com>
2020-02-29 15:35:00 +0530

    variable sum changed to sum1

--- addtion3.c
+++ addtion3.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 int main() {    
 
-    int number1, number2, sum;
+    int number1, number2, sum1;
     
     printf("Enter two integers: ");
     scanf("%d %d", &number1, &number2);
 
     // calculating sum
-    sum = number1 + number2;      
+    sum1 = number1 + number2;      
     
-    printf("%d + %d = %d", number1, number2, sum);
+    printf("%d + %d = %d", number1, number2, sum1);
     return 0;
 }
 
